f2347721abdb6ec21af1fdeb255b9154b41dd93e
spiffytech <spiffytech@gmail.com>
2014-04-11 04:44:07 +0000

    Initial version. Reads an XML file and prints all text messages in it.

--- smsviewer/__init__.py
+++ smsviewer/__init__.py
@@ -8,6 +8,6 @@ def main(global_config, **settings):
     config.add_renderer(".mako", "pyramid.mako_templating.renderer_factory")
     config.add_renderer(".html", "pyramid.mako_templating.renderer_factory")
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('index', '/')
     config.scan()
     return config.make_wsgi_app()
--- smsviewer/views.py
+++ smsviewer/views.py
@@ -1,6 +1,14 @@
 from pyramid.view import view_config
 
+from lxml import etree
 
-@view_config(route_name='home', renderer='hello.mako')
+@view_config(route_name='index', renderer='index.mako')
 def my_view(request):
+    smsfile = "sms.xml"
+    with open(smsfile) as f:
+        tree = etree.parse(f)
+
+    root = tree.getroot()
+
+    return {"messages": sorted([e for e in root if e.get("contact_name") == "Lacey Shankle"], key=lambda message: message.get("date"))}
     return {'project': 'smsviewer'}
