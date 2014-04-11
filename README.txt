e9cb13a634e3259e4bd983ca6e1dc55980a3ed68
spiffytech <spiffytech@gmail.com>
2014-04-11 04:19:07 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+smsviewer README
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid<1.5',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'lxml',
+    ]
+
+setup(name='smsviewer',
+      version='0.0',
+      description='smsviewer',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="smsviewer",
+      entry_points="""\
+      [paste.app_factory]
+      main = smsviewer:main
+      """,
+      )
